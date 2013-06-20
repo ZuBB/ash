11e160ed44869067e472e4b107eb5ee972068159
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-06-20 23:37:58 +0300

    infractructure
    This commit introduces stuff that helps to manage plugin developemnt
     * vcs
     * build stuff
     * validation
     * misc utils

---
+++ build/extern.js
@@ -0,0 +1,30 @@
+function ReportOut(a) {}
+function SetMarker(a, b) {}
+function CanContinue() {}
+function CreateGraphic(a, b, c) {}
+function ValueAt(a, c) {}
+function SynchByEx(a, b, c, d, e, f, g) {}
+function CreateGraphicView(a) {}
+function CreateConfigure(a) {}
+function GetCurLanguage() {}
+function Enumerator() {}
+
+var Frequency = null;
+var DataType = null;
+var Host = null;
+
+var stubObj = null;
+stubObj.AddItem = function(a) {};
+stubObj.Configure = function() {};
+stubObj.GetValue = function(a) {};
+stubObj.SetLimits = function(a, b) {};
+stubObj.SetScale = function(a, b) {};
+stubObj.AddPoint = function(a, b) {};
+stubObj.AddGraphicEx = function(a, b, c) {};
+stubObj.FileExists = function(a) {};
+stubObj.GetFile = function(a) {};
+stubObj.Delete = function(a, b) {};
+stubObj.CreateTextFile = function(a, b) {};
+stubObj.Close = function(a, b) {};
+stubObj.Write = function(a, b) {};
+
