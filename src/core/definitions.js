d4cc0f70f757a0b618ff81f1459e46bd3414cf37
Vasyl Zuzyak <zvasylvv@gmail.com>
2013-08-13 19:56:23 +0300

    lib/core: definitions
    This file contains declarations of variables that (will) hold
    core classed and theirs public functions

---
+++ src/core/definitions.js
@@ -0,0 +1,30 @@
+// global objects
+var Dispatcher = null;
+var Task       = null;
+var Input      = null;
+//DEBUG_START
+var Logger     = null;
+//DEBUG_STOP
+var Reporter   = null;
+var Profiler   = null;
+var Utils      = {};
+var MathHelper = {};
+
+// i18n
+var _t = null;
+
+//DEBUG_START
+// logger stuff
+var _d = null;
+var _i = null;
+var _p = null;
+var _w = null;
+var _e = null;
+var _f = null;
+//DEBUG_STOP
+
+// reporter stuff
+var _rh = null;
+var _rl = null;
+var _rp = null;
+
