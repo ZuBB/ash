df9d6c73eee01417c41aafeffbf246bb045943b6
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-02-25 17:27:52 +0200

    input: added JSDoc annotations
    Also switched to user-defined inputs and dialogs

--- src/core/input.js
+++ src/core/input.js
@@ -1,3 +1,138 @@
+/**
+ * Input module.
+ *
+ * Allows nice handling of tasks related to inputs, dialog
+ * and stuff related to it
+ *
+ * If script needs from user any external data it needs to show dialog(s)
+ * with input fields in it.
+ *
+ * By default inputs script does not know any inputs (see
+ * {@link Script#inputFields} property) and content of dialogs is also not set
+ * (see {@link Script#dialogsContent} property))
+ *
+ * To change that you need to define value of `Script.inputFields` property
+ * in next file
+ *
+ * ```
+ * src/app/input_fields.js
+ * ```
+ *
+ * This file may looks like this
+ *
+ * ```
+ * // A hash with all possible input fields that will ever will be used
+ * // in dialog(s) for this script
+ * Script.inputFields = {
+ *     'input1': {
+ *         type: 'int'
+ *     },
+ *     'input2': {
+ *         type: 'float'
+ *     },
+ *     'input3': {
+ *         type: 'string'
+ *     },
+ *     'input4': {
+ *         type: 'combobox',
+ *         value: ['inputs.combo.no', 'inputs.combo.yes']
+ *     },
+ *     'input5': {
+ *         value: function() {
+ *             return 'a' + 'b';
+ *         }
+ *     }
+ * };
+ * ```
+ *
+ * As you can see its a dictionary with key/value pairs
+ *
+ * Each key is an internal name of the input that is used everywhere in script.
+ * Even if this is not stated directly you need to keep in mind that.
+ *
+ * While specifying inputs you need to follow 2 simple rules:
+ *
+ * - internal name (key of dictionary) of input should be unique
+ * - dictionary which defines attributes of input should have at least of next
+ *   keys:
+ *
+ *      - `value` - default value
+ *      - `type` - default type
+ *
+ * For inputs you can specify one of 4 available data types:
+ *
+ * - `int` - integer numbers
+ * - `float` - number with floating poing
+ * - `string` - string
+ * - `combobox` - means this input will have form of dropdown with items that
+ *   **must** be specified as `value` property
+ *
+ * As for values here you have even more freedom:
+ *
+ * - `Number` - any of integer of floaft numbers
+ * - `String` - any string
+ * - `Array` - as we alread said its for combobox
+ * - `Function` - allows define default value that will be dynamically
+ *   calculated
+ * - **CONSTANT**s. You can even set default value to previously defined
+ *   constant from `src/app/constants.js` file (that's a user defined constants)
+ *
+ *
+ * Content of dialogs may be (re)defined as value of `Script.dialogsContent`
+ * variable in next file
+ *
+ * ```
+ * src/app/dialogs.js
+ * ```
+ *
+ * Here is an example of possible content for that file
+ *
+ * ```
+ * Script.dialogsContent = [
+ *     'input1',
+ *     Input.separator,
+ *     'input2',
+ *     'input3',
+ *     'input4',
+ *     'input5',
+ *     'input6',
+ *     'input7',
+ *     function(){
+ *         return Input.getValue('input1') === 'I want more!' ? [] : [
+ *             'input8',
+ *             'input9',
+ *         ];
+ *     },
+ *     'input0'
+ * ];
+ * ```
+ *
+ * As you can see 3 types of items are supported here:
+ *
+ * - name of the input
+ * - `Input.separator` This an alias to item that does next trick: adding it
+ *   to dialog is a tricky way to say "*its enough for this dialog to have
+ *   inputs, lets push next items into new dialog*". Also it causes current
+ *   dialog window to be shown and wait for user's input
+ * - anonymous function that should return an array. When
+ *   {@link Input#createConfiguration} meets function, it evaluates it and
+ *   replaces it with result that function returned. This allows dynamically
+ *   create dialog(s) content
+ *
+ * Dialog(s) are created and processed automatically if both
+ * {@link Script#dialogsContent} and {@link Script#inputFields} properties
+ * are not empty and have valid content
+ *
+ * To have inputs in dialogs automatically translated you need to add to
+ * resource localization file line like next for each input you have defined
+ * in your `src/app/input_fields.js` file
+ *
+ * ```
+ * inputs.YOUR_INPUT_INTERNAL_NAME.name = Введиде пожалуйста число
+ * ```
+ *
+ * @class
+ */
 Input = {
     dialogIndex:   0,
     input2dialogMap: {},
@@ -20,9 +155,13 @@ Input.DEFAULT_VALUE = {
 };
 
 /**
- * function that ...
+ * Returns default value for input
  *
- * @method getDefaultValue
+ * @param {String} name Internal name of the input
+ * @return {String} default value for input name we passed
+ *
+ * @member Input
+ * @private
  */
 Input.getDefaultValue = function(name) {
     if ('value' in this.possible_fields[name]) {
@@ -54,14 +193,20 @@ Input.getDefaultValue = function(name) {
 };
 
 /**
- * function that ...
+ * Creates dialog window(s), and shows them.
+ * Content of dialog(s) based on content of `Script.dialogsContent` variable
+ * Specs of input field(s) are taken from `Script.inputFields` variable
  *
- * @method createConfiguration
+ * @member Input
+ * @private
  */
-Input.createConfiguration = function(items, possibleInputs) {
-    if (Array.isArray(items) === false || items.length < 1) {
+Input.createConfiguration = function() {
+    var items = Script.dialogsContent;
+    var possibleInputs = Script.inputFields;
+
+    if (Array.isArray(items) === false) {
         //DEBUG_START
-        _w("Can't create configuration. Options is not an array or is empty");
+        _w("Can't create configuration. Options is not an array");
         //DEBUG_STOP
         return;
     }
@@ -112,9 +257,13 @@ Input.createConfiguration = function(items, possibleInputs) {
 };
 
 /**
- * function that ...
+ * Returns localized name of the input by its internal name
  *
- * @method getInputI18Name
+ * @param {String} name Internal name of the input
+ * @return {String} localized name
+ *
+ * @member Input
+ * @private
  */
 Input.getInputI18Name = function(name) {
     return this.possible_fields[name].hasOwnProperty('name') ?
@@ -122,9 +271,13 @@ Input.getInputI18Name = function(name) {
 };
 
 /**
- * function that ...
+ * Checks if given input is known for this script
  *
- * @method isInputNameKnown
+ * @param {String} name Internal name of the input
+ * @return {Boolean} localized name
+ *
+ * @member Input
+ * @private
  */
 Input.isInputNameKnown = function(name) {
     if (!name) {
@@ -144,9 +297,14 @@ Input.isInputNameKnown = function(name) {
 };
 
 /**
- * function that ...
+ * Returns raw value that was entered by user by given input name
+ * That value is not casted to type that was defined on input creation
  *
- * @method getRawValue
+ * @param {String} name Internal name of the input
+ * @return {Object} result
+ *
+ * @member Input
+ * @private
  */
 Input.getRawValue = function(name) {
     if (!this.isInputNameKnown(name)) {
@@ -173,9 +331,13 @@ Input.getRawValue = function(name) {
 };
 
 /**
- * function that ...
+ * Returns value that was entered by user by given input name
+ * That value is additionally casted to type that was defined on input creation
  *
- * @method getValue
+ * @param {String} name Internal name of the input
+ * @return {Object} result
+ *
+ * @member Input
  */
 Input.getValue = function(name) {
     var raw_value = this.getRawValue(name);
