d6c30a30823108baf38c68baad41002286fe42ba
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-02-26 13:44:21 +0200

    reporter: added JSDoc annotations

--- src/core/reporter.js
+++ src/core/reporter.js
@@ -1,16 +1,67 @@
-/*
- * Reporter lib. Inspired from Ruby's one
+/**
+ * Reporter library.
+ *
+ * [USB Oscilloscope](http://injectorservice.com.ua/home.php) application has
+ * ability to print messages into Report tab via built-in function
+ * `Host.ReportOut()`. This library provides 3 handy wrapper functions on top
+ * of that function. They are:
+ *
+ * - `_rw()` - prints passed string as it would be a words (no newlines
+ *   appended)
+ * - `_rl()` - prints passed string as it would be a sentence at the end of
+ *   paragraph (newline at the end appended)
+ * - `_rp()` - prints passed string as it would be a paragraph with single
+ *   sentence in it (newline at both sides appended)
+ *
+ * Each of these functions accepts next params
+ *
+ * @param {String} message Text that need to be printed
+ * @param {Object} [controlChars] A dictionary with different types of options
+ * that will turn plain text message into link to various resources of
+ * Oscilloscope app
+ * @param {Array} [controlChars.colors] An array with 1-2 numbers that
+ * represent colors:
+ *
+ * - 1st - color of the text
+ * - 2nd - background color
+ *
+ * For example see annotation of {@link Reporter#_insertColorsMark} method
+ *
+ * @param {Array} [controlChars.notation] An array with 2 numbers that
+ * represent 'address' of the graphic that should be pointed to
+ *
+ * - 1st - index of view
+ * - 2nd - index of graphic
+ *
+ * For example see annotation of {@link Reporter#_insertLink2Notation} method
+ *
+ * @param {Array} [controlChars.oscillogram] An array with 1 number that
+ * represent position at oscillogram that should be shown
+ *
+ * For example see annotation of {@link Reporter#_insertLink2OSCGRM} method
+ *
  */
 Reporter = {};
 
 /**
- * function that ...
+ * Function that actually does printing of messages in report tab. It is
+ * recommended to use wrapper functions (`_rw()`, `_rl()`, `_rp()`) instead
+ * of this one, but we can not forbid to use it. In that case you have to deal
+ * with set of all params for it
  *
- * seems that printing in _Report_ tab is slow
- * need to reduce number of messages that is being printed there
+ * @param {Boolean} b_lf Flag that indicates if newline symbols should be
+ * appended at start of message
+ * @param {Boolean} a_lf Flag that indicates if newline symbols should be
+ * appended at end message
+ * @param {String} value Item that should be printed
+ * @param {String} [description] Optional description of the value. This param
+ * is **deprecated** and with be removed in next versions
+ * @param {Object} [controlChars] Dictionary with control chars. See detailed
+ * description in Class annotation
  *
  *
- * @method report
+ * @member Reporter
+ * @private
  */
 Reporter.report = function(b_lf, a_lf, value, description, controlChars) {
     var rawString = null;
@@ -46,12 +97,23 @@ Reporter.report = function(b_lf, a_lf, value, description, controlChars) {
 };
 
 /**
- * function that makes text in report tab colorful
+ * Makes text in report tab colorful
  *
- * raw example: ReportOut("<ta=FFFFFF,0000FF [ Blue ]>");
- * lib example: _rl(_t('core.graphic.error1'), {colors: [0xFFFFFF, 0xFF0000]});
+ * See example:
  *
- * @method report
+ * ```
+ * // raw example
+ * ReportOut("<ta=FFFFFF,0000FF Blue>");
+ * // example with help of wrapper function
+ * _rw('Blue', {colors: [0xFFFFFF, 0xFF0000]});
+ * ```
+ *
+ * @param {String} string Text that should be coloured
+ * @param {Array} colors An array of colors
+ * @return {string} crafted string with appended color pseudocodes
+ *
+ * @member Reporter
+ * @private
  */
 Reporter._insertColorsMark = function(string, colors) {
     function dumbColor(color) {
@@ -82,12 +144,24 @@ Reporter._insertColorsMark = function(string, colors) {
 };
 
 /**
- * function that converts text in report tab into link that
- * shows view YYY, and activate graphic comment ZZZ
+ * Converts text in report tab into link that shows view YYY, and activate
+ * graphic comment ZZZ
  *
- * example: ReportOut('<sg=1,2 Activate Graphic>\n')
+ * See example:
  *
- * @method _insertLink2Notation
+ * ```
+ * // raw example
+ * ReportOut('<sg=1,2 Activate Graphic>\n')
+ * // example with help of wrapper function
+ * _rl('Activate Graphic', {notation: [1, 2]});
+ * ```
+ *
+ * @param {String} string Text that should be coloured
+ * @param {Array} notation An array of notation options
+ * @return {string} crafted string with appended notation pseudocodes
+ *
+ * @member Reporter
+ * @private
  */
 Reporter._insertLink2Notation = function(string, notation) {
     if (!string || !(notation && Array.isArray(notation))) {
@@ -105,12 +179,23 @@ Reporter._insertLink2Notation = function(string, notation) {
 };
 
 /**
- * function that converts text in report tab in to link that
- * shows oscillogram position at ZZZ
+ * Converts text in report tab in to link that shows oscillogram at position X
  *
- * example: ReportOut("\n<sp=0 Open oscillogram>\n");
+ * See example:
  *
- * @method _insertLink2OSCGRM
+ * ```
+ * // raw example
+ * ReportOut("\n<sp=0 Open oscillogram>\n");
+ * // example with help of wrapper function
+ * _rp('Open oscillogram', {oscillogram: [0]});
+ * ```
+ *
+ * @param {String} string Text that should be coloured
+ * @param {Array} oscillogram An array of oscillogram options
+ * @return {string} crafted string with appended oscillogram pseudocodes
+ *
+ * @member Reporter
+ * @private
  */
 Reporter._insertLink2OSCGRM = function(string, oscillogram) {
     if (!string || !(oscillogram && Array.isArray(oscillogram))) {
@@ -129,16 +214,36 @@ Reporter._insertLink2OSCGRM = function(string, oscillogram) {
 
 // ==========================================
 
+/**
+ * Prints message to report tab. Any newline symbols will not be appended
+ *
+ * @member Reporter
+ * method _rp
+ */
 _rw = function() {
     Reporter.report.apply(Reporter,
         Utils.prepareParams(arguments, false, false));
 };
 
+/**
+ * Prints message to report tab. To the end of message newline symbol will be
+ * appended
+ *
+ * @member Reporter
+ * method _rl
+ */
 _rl = function() {
     Reporter.report.apply(Reporter,
         Utils.prepareParams(arguments, true, false));
 };
 
+/**
+ * Prints message to report tab. To the end of message newline symbol will be
+ * appended. Also newline symbol will be inserted before message
+ *
+ * @member Reporter
+ * @method _rp
+ */
 _rp = function() {
     Reporter.report.apply(Reporter,
         Utils.prepareParams(arguments, true, true));
