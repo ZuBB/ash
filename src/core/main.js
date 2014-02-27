57f5fb1ad178b72e34728dd9cc00ce76b613c0b5
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-02-27 19:28:00 +0200

    main: comment out init method

--- src/core/main.js
+++ src/core/main.js
@@ -1,6 +1,6 @@
 if (Host.DataType === '$DATATYPE$') {
     // init dispatcher with options
-    Dispatcher.init(Script.dispatcherOpts);
+    //Dispatcher.init(Script.dispatcherOpts);
 
     // function that does all the magic
     Dispatcher.process();
