308e89b197911137e8c59144b4ae7d6386e35fa1
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-03-05 19:36:10 +0200

    infrastucture

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,58 +16,11 @@ module.exports = function(grunt) {
                     'external': ['DataSet']
                 }
             }
-        },
-        gitcommit: {
-            commit: {
-                options: {
-                    message: 'JSDocs updated'
-                },
-                files: {
-                    src: [
-                        'data-*.js',
-                        'index.html',
-                        'output/*.js',
-                        'source/*.html'
-                    ]
-                }
-            }
-        },
-        gitpush: {
-            push: {
-                options: {
-                    branch: 'master'
-                }
-            }
         }
     });
 
     grunt.loadNpmTasks('grunt-jsduck');
-    grunt.loadNpmTasks('grunt-git');
 
-    grunt.config.set('work-dir', {
-        'gwd': process.cwd(),
-        'twd': grunt.config.get('jsduck.main.dest')
-    });
-
-    grunt.registerTask('work-dir', 'change/restore current working dir', function() {
-        grunt.log.writeln(grunt.config.get('work-dir.gwd'));
-        grunt.log.writeln(grunt.config.get('work-dir.twd'));
-    });
-
-    grunt.registerTask('work-dir:change', 'change current working dir', function() {
-        grunt.file.setBase(grunt.config('work-dir.twd'));
-    });
-
-    grunt.registerTask('work-dir:restore', 'restore current working dir', function() {
-        grunt.file.setBase(grunt.config.get('work-dir.gwd'));
-    });
-
-    grunt.registerTask('work-dir:pwd', 'print current working dir', function() {
-        grunt.log.writeln(process.cwd());
-    });
-
-    grunt.registerTask('git', ['gitcommit', 'gitpush']);
-    grunt.registerTask('jsdoc', ['jsduck:main', 'work-dir:change', 'git', 'work-dir:restore']);
-    grunt.registerTask('default', ['jsdoc']);
+    grunt.registerTask('default', ['jsduck:main']);
 };
 
