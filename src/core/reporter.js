e38a694e1ad949f9e6c7414e265af8d577547f27
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-03-06 17:08:59 +0200

    reporter: stop using internal prop of Logger

--- src/core/reporter.js
+++ src/core/reporter.js
@@ -87,7 +87,7 @@ Reporter.report = function(b_lf, a_lf, value, description, controlChars) {
         Host.ReportOut(string);
 //DEBUG_START
         // 2nd condition: no need to log status of specs
-        if (Logger._buffering && !(/[\+\-]{1}\n/).test(rawString)) {
+        if (!(/[\+\-]{1}\n/).test(rawString)) {
             rawString = rawString.replace(/^\n/, '');
             rawString = rawString.replace(/\n$/, '');
             _i(rawString);
