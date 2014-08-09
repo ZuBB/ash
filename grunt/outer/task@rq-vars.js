3a2a6b8815ab44f0f83aebac16bcfed926ea9f18
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-08-09 12:35:53 +0300

    grunt[rq-vars subtask]: add loading of excludes from external file

--- grunt/outer/task@rq-vars.js
+++ grunt/outer/task@rq-vars.js
@@ -2,5 +2,10 @@ module.exports = function(grunt) {
     grunt.registerTask('rq-vars', function() {
         grunt.config.set('vars.buildType', 'release');
         grunt.config.set('vars.graphic_type', 0);
+
+        if (grunt.file.exists('excludes_list.json')) {
+            grunt.config('vars.excludesList',
+                grunt.file.readJSON('excludes_list.json').excludesList);
+        }
     });
 };
