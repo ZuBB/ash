29055d2ed07b2c7e42ada0372f962defc4836f84
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-08-27 14:29:35 +0300

    jshint: move options from external file to task

--- grunt/outer/jshint.js
+++ grunt/outer/jshint.js
@@ -1,10 +1,59 @@
+// http://www.jshint.com/docs/options/
+// http://jslinterrors.com/
+
 module.exports = {
     options: {
-        reporter: require('jshint-stylish')
+        reporter: require('jshint-stylish'),
+
+        globals: {
+            'ActiveXObject': false,
+            'Enumerator': false,
+            'Host': false
+        },
+
+        es3: true,
+        nonbsp: true,
+        undef: true,
+        loopfunc: true,
     },
-    main: {
-        jshintrc: 'src/core/jshintrc',
+    core: {
+        src: 'src/core/src/core/*js',
+        options: {
+            curly: true,
+            eqeqeq: true,
+            forin: true,
+
+            indent: 4,
+            maxdepth: 3,
+            maxlen: 80,
+
+            latedef: true,
+            unused: true,
+            multistr: true,
+            passfail: false,
+            loopfunc: false,
+        }
+    },
+    light: {
         src: '<%= vars.destFile %>'
+    },
+    moderate: {
+        src: '<%= vars.destFile %>',
+        options: {
+            curly: true,
+            eqeqeq: true,
+            forin: true,
+
+            indent: 4,
+            maxdepth: 3,
+            maxlen: 80,
+
+            latedef: true,
+            unused: true,
+            multistr: true,
+            passfail: false,
+            loopfunc: false,
+        }
     }
 };
 
