066756a832af52a761dbd00a4d44efc3727350ad
Vasyl Zuzyak <zvasylvv@gmail.com>
2014-09-25 13:07:31 +0300

    grunt[watch] do linf of i18n files before encoding

--- common/grunt/watch.js
+++ common/grunt/watch.js
@@ -1,6 +1,18 @@
 module.exports = {
-    i18n: {
-        files: '**\/lang-ru.js',
+    'i18n-jshint': {
+        files: [
+            'src/core/src/lang/init-lang.js',
+            'src/core/src/lang/lang-??.js',
+            'src/lang/lang-??.js',
+        ],
+        tasks: ['jshint:i18n'],
+        options: {
+            atBegin: true,
+            event: ['changed']
+        }
+    },
+    'i18n-transcode': {
+        files: ['src/core/src/lang/lang-ru.js', 'src/lang/lang-ru.js'],
         tasks: ['transcode:i18n'],
         options: {
             atBegin: true,
