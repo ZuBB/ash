0eac4bf0c2f751615ad006995da3597c546aa129
Vasyl Zuzyak <zvasylvv@gmail.com>
2015-02-11 03:26:29 +0200

    refactor(build): use data from new config file

--- common/grunt/task@vars.js
+++ common/grunt/task@vars.js
@@ -2,19 +2,17 @@ module.exports = function(grunt) {
     grunt.registerTask('dev-vars', function() {
         grunt.config.set('vars.buildType', 'dev');
         grunt.config.set('vars.graphic_type', 2);
-        grunt.config.set('vars.name', grunt.config('pkg.name'));
-        grunt.config.set('vars.ext', 'ajs');
+        grunt.config.set('vars.name', grunt.config('pkg.devFilename'));
     });
 
     grunt.registerTask('rq-vars', function() {
         grunt.config.set('vars.buildType', 'release');
         grunt.config.set('vars.graphic_type', 0);
-        grunt.config.set('vars.name', grunt.config('pkg.title'));
+        grunt.config.set('vars.name', grunt.config('pkg.releaseFilename') ||
+            grunt.config('pkg.devFilename'));
 
-        if (grunt.file.exists('excludes_list.json')) {
-            grunt.config('vars.excludesList',
-                grunt.file.readJSON('excludes_list.json').excludesList);
-        }
+        grunt.config('vars.excludesList',
+                grunt.config('pkg.excludesList') || []);
     });
 
     grunt.registerTask('test-vars', function() {
