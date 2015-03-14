84d7be7ac34c07c2ad361dfcd6b5c52019c50c9c
Vasyl Zuzyak <zvasylvv@gmail.com>
2015-03-14 11:16:13 +0200

    chore(build): merge vars tasks

--- common/grunt/vars.js
+++ common/grunt/vars.js
@@ -1,36 +1,61 @@
-module.exports = {
-    'ext': 'ajs',
-    'name': '',
-    'dest': 'build/output/',
-    'buildType': null,
-    'excludesList': [],
+module.exports = function(grunt) {
+    grunt.registerTask('dev-vars', function() {
+        grunt.config.set('vars.buildType', 'dev');
+        grunt.config.set('vars.graphic_type', 2);
+        grunt.config.set('vars.name', grunt.config('pkg.devFilename'));
+    });
 
-    'destFile': [
-        '<%= vars.dest %>',
-        '<%= vars.name.toLowerCase() %>.',
-        '<%= vars.buildType %>.',
-        '<%= vars.ext %>'
-    ].join(''),
+    grunt.registerTask('rq-vars', function() {
+        grunt.config.set('vars.buildType', 'release');
+        grunt.config.set('vars.graphic_type', 0);
+        grunt.config.set('vars.name', grunt.config('pkg.releaseFilename') ||
+            grunt.config('pkg.devFilename'));
 
-    'mail': {
-        'to': '',
-        'cc': '',
-        'bcc': '',
-        'from': 'ASH Build System',
-        'subject': '',
-        'message': '',
-        'messageFooter': 'Email sent by ASH robat v2.',
-        'smtpCredsFile': 'creds.smtp.json'
-    },
+        grunt.config('vars.excludesList',
+                grunt.config('pkg.excludesList') || []);
+    });
 
-    'hash': null,
-    'branch': null,
-    'modified': null,
-    'changesets': null,
+    grunt.registerTask('test-vars', function() {
+        //grunt.config.set('vars.createGraphics', false);
+        grunt.config.set('vars.logLevel', 1);
+    });
 
-    'datatype': '$DATATYPE$',
-    'demo_mode': false,
-    'dump_tasks_data': false,
-    'graphic_type': null,
-    'logLevel': 0
+    return {
+        'ext': 'ajs',
+        'name': '',
+        'dest': 'build/output/',
+        'buildType': null,
+        'excludesList': [],
+
+        'destFile': [
+            '<%= vars.dest %>',
+            '<%= vars.name.toLowerCase() %>.',
+            '<%= vars.buildType %>.',
+            '<%= vars.ext %>'
+        ].join(''),
+
+        'mail': {
+            'to': '',
+            'cc': '',
+            'bcc': '',
+            'from': 'ASH Build System',
+            'subject': '',
+            'message': '',
+            'messageFooter': 'Email sent by ASH robat v2.',
+            'smtpCredsFile': 'creds.smtp.json'
+        },
+
+        'hash': null,
+        'branch': null,
+        'modified': null,
+        'changesets': null,
+
+        'datatype': '$DATATYPE$',
+        'demo_mode': false,
+        'dump_tasks_data': false,
+        'graphic_type': null,
+        'logLevel': 0
+    };
 };
+
+
