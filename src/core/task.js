ZuBB/ash

commit 7dfaa2b50b2baf626373b55e58214221d8e0e90e

fix(task): do a proper check of unknown input

src/core/task.js
@@ -498,7 +498,7 @@ Task.prototype.checkDataSource = function() {
     if (this.dataSource) {
         var dataSource = Input.getValue(this.dataSource);
 
-        if (dataSource === null) {
+        if (typeof dataSource === 'undefined') {
             //DEBUG_START
             _w(this.dataSource, 'dataSource is not valid');
             //DEBUG_STOP

commit e5b029147543a9c58d427f6b8d2a93ac47fccee4

fix(task): fix(task): do full set of checks for unknown/empty inputs
This change fixes a bug that was introduced by 7dfaa2b changeset.
In it we tried to fix another bug :)
Now we cover two options

src/core/task.js
@@ -498,7 +498,11 @@ Task.prototype.checkDataSource = function() {
     if (this.dataSource) {
         var dataSource = Input.getValue(this.dataSource);
 
-        if (typeof dataSource === 'undefined') {
+        // check for null is required for case when
+        //      number of channel is requested that is unset by user
+        // check for undefined is required for case when
+        //      we need to get a value of input that was not shown to user
+        if (typeof dataSource === 'undefined' || dataSource === null) {
             //DEBUG_START
             _w(this.dataSource, 'dataSource is not valid');
             //DEBUG_STOP