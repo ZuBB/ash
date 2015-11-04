fe72d5e950aa0dea7bb455a0168ea1e18acd7fed
Vasyl Zuzyak <zvasylvv@gmail.com>
2015-11-04 09:06:01 +0200

    feat(build): rework 'prev_file' to support 'platform' feature
    Also with this commit we stop using 'grunt-prev_file' plugin

--- common/grunt/prev_file.js
+++ common/grunt/prev_file.js
@@ -1,17 +1,39 @@
 module.exports = function(grunt, options) {
-    return {
-        script: {
-            createFilename: function(currentTask) {
-                var parts = [
-                    grunt.config('vars.dest'),
-                    grunt.config('vars.name').toLowerCase(),
-                    '.'+ currentTask.name + '.',
-                    grunt.config('vars.ext')
-                ];
+    grunt.registerTask('prev_file', function() {
+        //console.log(this)
+        var destFilenames = this._destFilenames = {};
 
-                return parts.join('');
+        grunt.config(this.name + '.getNewDestFile', function() {
+            var platform = grunt.config('vars.platform');
+            var parts = [
+                grunt.config('vars.dest'),
+                grunt.config('vars.name').toLowerCase(),
+                '.' + grunt.task.current.name,
+                '.' + platform + '.',
+                grunt.config('vars.ext')
+            ];
+
+            var newFilename = parts.join('');
+
+            if (destFilenames.hasOwnProperty(platform) === false) {
+                destFilenames[platform] = [];
             }
-        }
-    };
+
+            if (destFilenames[platform].indexOf(newFilename) < 0) {
+                destFilenames[platform].push(newFilename);
+            }
+
+            return newFilename;
+        });
+
+        grunt.config(this.name + '.getLastDestFile', function() {
+            return destFilenames[grunt.config('vars.platform')]
+                .filter(function(filepath) {
+                    return grunt.file.exists(filepath);
+                })
+                .slice(-1)
+                .pop();
+        });
+    });
 };
 
